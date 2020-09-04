e6500a8e32b2ab6ca2d6e9b790f569b23b2704ba
Shirley Cohen <scohen@cs.utexas.edu>
2020-09-04 09:41:28 -0500

    class exercise

---
+++ create_college_staging.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS college;
+CREATE DATABASE college;
+USE college;
+
+-- create Classes
+-- tid,instructor,dept,cno,cname,credits
+CREATE TABLE Classes(
+    tid varchar(50),
+    instructor varchar(200),
+    dept varchar(50),
+    cno char(6),
+    cname varchar(200),
+    credits int
+);
+
+-- create Current_Students
+-- sid,fname,lname,dob,cno,cname,credits,grade
+CREATE TABLE Current_Students(
+    sid varchar(50),
+    fname varchar(200),
+    lname varchar(200),
+    dob varchar(20),
+    cno char(6),
+    cname varchar(200),
+    credits int,
+    grade char(2)
+);
+
+-- create New_Students
+-- sid,fname,lname,dob
+CREATE TABLE New_Students(
+    sid varchar(50),
+    fname varchar(200),
+    lname varchar(200),
+    dob date
+);
